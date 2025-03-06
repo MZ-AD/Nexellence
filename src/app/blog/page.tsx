e21555b1249c2946488edd9bad7c71644bd20e6d
Moeez Ahmed <x311099@gmail.com>
2025-03-06 14:28:21 +0500

    changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,9 +5,9 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Blog Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Blog Page for Startup Nextjs Template",
-  // other metadata
+  title: "",
+  description: "",
+  
 };
 
 const Blog = () => {
